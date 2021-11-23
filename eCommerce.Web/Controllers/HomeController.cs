4c72baaadc353f339d2a171862766174efd5a7ca
David Adamson <david@212digital.co.uk>
2021-11-23 14:07:01 +0000

    fixed overflow issue and images not displaying when using overflow-x in body tag

--- eCommerce.Web/Controllers/HomeController.cs
+++ eCommerce.Web/Controllers/HomeController.cs
@@ -59,7 +59,7 @@ namespace eCommerce.Web.Controllers
                     model.CategoryName = selectedCategory.SanitizedName;
                     model.SelectedCategory = selectedCategory;                    
                     model.SearchedCategories = CategoryHelpers.GetAllCategoryChildrens(selectedCategory, model.Categories);
-                   
+                                       
                 }
             }
             
@@ -70,6 +70,7 @@ namespace eCommerce.Web.Controllers
             model.PageSize = recordSize;
           
             var selectedCategoryIDs = model.SearchedCategories != null ? model.SearchedCategories.Select(x => x.ID).ToList() : null;
+            
 
             model.Products = ProductsService.Instance.SearchProducts(selectedCategoryIDs, model.SearchTerm, model.PriceFrom, model.PriceTo, model.SortBy, pageNo, recordSize.Value, activeOnly: true, out int count, stockCheckCount: null);
 
